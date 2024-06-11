591d8430adfcac8632b89403db55c49ebc70b746
sadiqkhzn <sadiqkhan795@gmail.com>
2024-06-11 20:44:26 +0530

    rightsidebar, and pages

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,11 @@
 import HeaderBox from '@/components/HeaderBox';
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
-  const loggedIn = { firstName: 'Sadiq'};
+  const loggedIn = { firstName: 'Sadiq', lastName: 'Khan', 
+    email: 'sadiqkhan795@gmail.com'};
 
   return (
     <section className="home">
@@ -22,7 +24,14 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{}, {}]}
+      />
     </section>
   )
 }
